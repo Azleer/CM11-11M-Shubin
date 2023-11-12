72d9ec40a9db6cb1343b933842dc38ecf9e2376f
Azleer <84662653+Azleer@users.noreply.github.com>
2023-11-12 20:12:22 +0300

    .

--- List/main.cpp
+++ List/main.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
-    MyList<int> L;
+    MyList<int> Lhs, Rhs;
+    Lhs.push_back(2); Lhs.push_back(4); Lhs.push_back(6); Lhs.push_back(8); Lhs.push_back(12);
+    Rhs.push_back(1); Rhs.push_back(3); Rhs.push_back(13);
 
-    for (int var = 0; var < 20; ++var)
-    {
-        L.push_back(var);
-    }
 
-    L.print();
+    Lhs.smerge(&Rhs);
+
+    MyList<int> temp;
+
+
 
     return 0;
 }
