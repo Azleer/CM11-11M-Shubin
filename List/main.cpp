fef52fccf09175df12834ca5715711e4306244c2
Azleer <84662653+Azleer@users.noreply.github.com>
2023-12-09 14:55:24 +0300

    Update main.cpp

--- List/main.cpp
+++ List/main.cpp
@@ -9,12 +9,7 @@ int main()
     Lhs.push_back(1); Lhs.push_back(2); Lhs.push_back(2); Lhs.push_back(1);  Lhs.push_back(2); Lhs.push_back(4); Lhs.push_back(15); Lhs.push_back(0); Lhs.push_back(100);  Lhs.push_back(2);
     Rhs.push_back(1); Rhs.push_back(3); Rhs.push_back(12); Rhs.push_back(14);
 
-    //Lhs.sort();
-
-
-        std::cout << "\n" << Lhs.is_sort() << "\n";
-        Lhs.bublesort();
-
+    Lhs.bublesort();
     Lhs.print();
 
 
